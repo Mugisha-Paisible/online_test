b9df38c3924e1ccafd33eecdd397bed0362d6977
DESKTOP-6LK0V75\user <paisibmugisha@gmail.com>
2021-03-06 06:43:27 -0800

    adding a few more packages for the backend and configuring questions route

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000
 const path = require('path')
+const db = require('./db/index.js')
 const bodyParser = require('body-parser')
+
+const questionRoutes = require('./routes/questions')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/views'));
 
-app.get('/views', function (req, res) {
-  //return res.render('index.html')
-  //return res.sendFile('index.html', {root : __dirname + '/views'});
+app.get('/', async function (req, res) {
   return res.sendFile(__dirname + '/views/index.html');
 })
+
+app.use('/questions', questionRoutes)
 
 // port
 app.listen(port, () => {
